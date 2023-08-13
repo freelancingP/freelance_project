216bb056d5f782370fec4371fc5b7096f02b8340
Manish Kushwaha <kushwahamanish818@gmail.com>
2023-08-13 15:00:24 +0530

    chbwgfewy

--- customer_engine/serializers.py
+++ customer_engine/serializers.py
@@ -11,6 +11,17 @@ class VerifyOtpSerializer(serializers.Serializer):
     
 
 class CustomerSerializer(serializers.ModelSerializer):
+    # Define a read-only field for the S3 bucket image URL
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def get_image_url(self, obj):
+        # Replace 'appstacklabs' with your actual S3 bucket name
+        bucket_name = 'appstacklabs'
+        image_name = obj.image.name.split('/')[-1]  # Extract the image file name
+        image_url = f'https://appstacklabs.s3.us-east-2.amazonaws.com/images/{image_name}'
+        print(image_url)
+        return image_url
