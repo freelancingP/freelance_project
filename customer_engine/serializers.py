cda9054cfcb1bac87a5e13807fca7fe875e081ee
Manish Kushwaha <kushwahamanish818@gmail.com>
2023-08-16 22:08:34 +0530

    jwebhge

--- customer_engine/serializers.py
+++ customer_engine/serializers.py
@@ -10,7 +10,15 @@ class VerifyOtpSerializer(serializers.Serializer):
     otp = serializers.CharField(max_length=6)
     
 
+class CustomListField(serializers.ListField):
+    def to_representation(self, data):
+        if isinstance(data, str):
+            return [item.strip() for item in data.split(',')]
+        return data
+
 class CustomerSerializer(serializers.ModelSerializer):
+    health_issue = CustomListField()
+    other_issue = CustomListField()
 
     class Meta:
         model = Customer
