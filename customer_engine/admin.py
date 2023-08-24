db9383f9825864e9c86c8374d779897d16f1bbbe
Manish Kushwaha <kushwahamanish818@gmail.com>
2023-08-24 17:18:25 +0530

    sde

--- customer_engine/admin.py
+++ customer_engine/admin.py
@@ -3,7 +3,10 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Customer)
-admin.site.register(Dishes)
+admin.site.register(Breakfast)
+admin.site.register(Launch)
+admin.site.register(Dinner)
+admin.site.register(Snacks)
 admin.site.register(UserOTP)
 admin.site.register(AddRecipe)
 admin.site.register(AddIngridient)
--- customer_engine/urls.py
+++ customer_engine/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('update/user/details/',views.UpdateUserDetailViews.as_view()),
     path('upload/image/',views.UploadImageView.as_view()),
     path('api/dish/calculator/',views.DishCalculatorViews.as_view()),
+    path('all/dishes/',views.AllDishesViews.as_view()),
+
 ]
