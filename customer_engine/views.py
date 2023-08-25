11936b2cdc94106c8ecef90e963710f3e79d08bc
Manish Kushwaha <kushwahamanish818@gmail.com>
2023-08-25 16:00:33 +0530

    dhwfet

--- customer_engine/views.py
+++ customer_engine/views.py
@@ -262,84 +262,6 @@ class UpdateUserDetailViews(APIView):
             }
             return Response(response_data)
 
-class DishCalculatorViews(APIView):   
-    def post(self, request):
-        auth_header = request.headers.get('Authorization')
-        data = request.data
-        if auth_header and auth_header.startswith('Bearer '):
-            token = auth_header.split(' ')[1]
-            
-            try:
-                decoded_payload = jwt.decode(token, algorithms=['HS256'], options={"verify_signature": False})
-                try:
-                    customer = Customer.objects.get(contact_number=decoded_payload['client_id'])
-                    print(data)
-                    recipe = AddRecipe.objects.get(item_name=data["item_name"])
-                    ingredient = AddIngridient.objects.filter(item = recipe)
-                    remain_quantity = recipe.quantity - int(data["item_quantity"])
-                    print(remain_quantity)
-                    if recipe.quantity > float(data["item_quantity"]):
-                        response_data = {
-                            "data": remain_quantity,
-                            "status": True,
-                            "code": 200,
-                            "message": f"You need to increase diet {remain_quantity} grams.",
-                        }
-                    elif recipe.quantity < float(data["item_quantity"]):
-                        response_data = {
-                            "data": remain_quantity,
-                            "status": True,
-                            "code": 200,
-                            "message": f"You need to reduce diet {remain_quantity} grams.",
-                        }
-                    else:
-                        response_data = {
-                            "data": remain_quantity,
-                            "status": True,
-                            "code": 200,
-                            "message": "Your diet maintained.",
-                        }
-                    return Response(response_data)
-                except Customer.DoesNotExist:
-                    response_data ={
-                        "data": None,
-                        "status": False,
-                        "code": 401,
-                        "message": "Invalid Authentication.",
-                    }
-                    return Response(response_data)
-                except Exception as e:
-                    response_data ={
-                        "data": None,
-                        "status": False,
-                        "code": 500,
-                        "message": "An error occurred.",
-                    }
-                    return Response(response_data)
-            except jwt.ExpiredSignatureError:
-                response_data ={
-                    "data": None,
-                    "status": False,
-                    "code": 401,
-                    "message": "Token has expired.",
-                }
-                return Response(response_data)
-            except jwt.DecodeError:
-                response_data ={
-                    "data": None,
-                    "status": False,
-                    "code": 498,
-                    "message": "Invalid token.",
-                }
-                return Response(response_data)
-        else:
-            response_data ={
-                "data": None,
-                "status": False,
-                "code": 400,
-                "message": "No token provided.",
-            }
-            return Response(response_data)
 
 
 class AllDishesViews(APIView):   
@@ -443,7 +365,62 @@ class GetDisheViews(APIView):
                             snacks_instances = Snacks.objects.all()  # Retrieve all Snacks instances
                             serializer = SnacksSerializer(snacks_instances)
                         response_data = {
-                            "data": serializer.data,
+                            "data":  {
+                                "caloriesUsed": 650,
+                                "calorieBreakdown": {
+                                "calories": 100,
+                                "pral": 100,
+                                "calcium": 450
+                                },
+                                "breakfast": [
+                                {
+                                    "dishName": "idli",
+                                    "ingredients": "Idli 100 gms idly Ravva, Water",
+                                    "calories": 400
+                                },
+                                {
+                                    "dishName": "idli",
+                                    "ingredients": "Idli 100 gms idly Ravva, Water",
+                                    "calories": 400
+                                }
+                              ],
+                              "lunch": [
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                },
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                }
+                              ],
+                              "eveningSnacks": [
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                },
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                }
+                              ],
+                              "dinner": [
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                },
+                                {
+                                  "dishName": "idli",
+                                  "ingredients": "Idli 100 gms idly Ravva, Water",
+                                  "calories": 400
+                                }
+                              ]
+                            },
                             "status": True,
                             "code": 200
                         }
@@ -457,6 +434,93 @@ class GetDisheViews(APIView):
                             "message": "Meal or Food Not Foud.",
                         }
                         return Response(response_data)
+                except Customer.DoesNotExist:
+                    response_data ={
+                        "data": None,
+                        "status": False,
+                        "code": 401,
+                        "message": "Invalid Authentication.",
+                    }
+                    return Response(response_data)
+                except Exception as e:
+                    print
+                    response_data ={
+                        "data": None,
+                        "status": False,
+                        "code": 500,
+                        "message": "An error occurred.",
+                    }
+                    return Response(response_data)
+            except jwt.ExpiredSignatureError:
+                response_data ={
+                    "data": None,
+                    "status": False,
+                    "code": 401,
+                    "message": "Token has expired.",
+                }
+                return Response(response_data)
+            except jwt.DecodeError:
+                response_data ={
+                    "data": None,
+                    "status": False,
+                    "code": 498,
+                    "message": "Invalid token.",
+                }
+                return Response(response_data)
+        else:
+            response_data ={
+                "data": None,
+                "status": False,
+                "code": 400,
+                "message": "No token provided.",
+            }
+            return Response(response_data)
+
+
+class UpdateDailyRecipeViews(APIView):   
+    def post(self, request):
+        auth_header = request.headers.get('Authorization')
+        data = request.data
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+            
+            try:
+                decoded_payload = jwt.decode(token, algorithms=['HS256'], options={"verify_signature": False})
+                try:
+                    customer = Customer.objects.get(contact_number=decoded_payload['client_id'])
+                    try:
+                        customer_id = request.data.get('customer')  # Get customer ID from the request data
+                        print(customer_id)
+                        customer_instance = get_object_or_404(Customer, id=customer_id)
+                        
+                        # Create a DailyRecipe instance and populate its fields
+                        daily_recipe_instance = DailyRecipe(
+                            customer=customer_instance,
+                            food=request.data.get('food'),
+                            quantity=request.data.get('quantity'),
+                            oil=request.data.get('oil'),
+                            gl=request.data.get('gl'),
+                            usable_cals=request.data.get('usable_cals'),
+                            # ... populate other fields ...
+                        )
+                        print(daily_recipe_instance)
+                        daily_recipe_instance.save()  # Save the instance
+                        
+                        serializer = DailyRecipeSerializer(daily_recipe_instance)
+                        response_data = {
+                            "data": serializer.data,
+                            "status": True,
+                            "code": 200
+                        }
+                        return Response(response_data)
+                    except Exception as e:
+                        response_data ={
+                            "data": None,
+                            "status": False,
+                            "code": 401,
+                            "message": "Meal or Food Not Found.",
+                        }
+                        return Response(response_data, status=status.HTTP_401_UNAUTHORIZED)
                 except Customer.DoesNotExist:
                     response_data ={
                         "data": None,
